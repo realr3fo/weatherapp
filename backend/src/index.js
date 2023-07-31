d8d099407b257c6723651460133196393dce916b
Refo Ilmiya <refo.ilmiya@gmail.com>
2023-08-01 01:18:03 +0300

    Added forecast API

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,11 +21,47 @@ const fetchWeather = async () => {
   return response ? response.json() : {};
 };
 
+const fetchForecastWeather = async (latitude, longitude) => {
+  const endpoint = `${mapURI}/forecast?lat=${latitude}&lon=${longitude}&appid=${appId}&`;
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
+};
+
 router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+router.get('/api/weather/forecast', async ctx => {
+  const { latitude, longitude } = ctx.request.query;
+
+  if (!latitude || !longitude) {
+    ctx.status = 400; // Bad request
+    ctx.body = { error: 'Missing latitude and/or longitude parameters' };
+    return;
+  }
+
+  const weatherData = await fetchForecastWeather(latitude, longitude);
+
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData.list
+    ? weatherData.list.map(x => ({
+      date_time_text: x.dt_txt,
+      weather_id: x.weather[0].id,
+      weather_main: x.weather[0].main,
+      weather_icon: x.weather[0].icon,
+      temperature: x.main.temp,
+      feels_like: x.main.feels_like,
+      wind_speed: x.wind.speed,
+      wind_deg: x.wind.deg,
+      wind_gust: x.wind.gust,
+      rain_probability: x.pop,
+      rain: x.rain,
+    }))
+    : [];
 });
 
 app.use(router.routes());
