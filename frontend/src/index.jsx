b856363daa22db01c6a69db9b424f2489205d67d
Refo Ilmiya <refo.ilmiya@gmail.com>
2023-08-01 14:54:46 +0300

    Implemented frontned for weatherapp

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,44 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const baseURL = process.env.ENDPOINT;
-
-const getWeatherFromApi = async () => {
-  try {
-    const response = await fetch(`${baseURL}/weather`);
-    return response.json();
-  } catch (error) {
-    // TODO: Handle error
-    console.error(error);
-  }
-
-  return {};
-};
-
-class Weather extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      icon: '',
-    };
-  }
-
-  async componentDidMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({ icon: weather.icon.slice(0, -1) });
-  }
-
-  render() {
-    const { icon } = this.state;
-
-    return (
-      <div className="icon">
-        {icon && <img src={`/img/${icon}.svg`} alt="weather icon" />}
-      </div>
-    );
-  }
-}
+import Weather from './Weather';
 
 ReactDOM.render(
   <Weather />,
