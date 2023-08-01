e6ef01fea548951aafd5ac18a90a5a70f77ef1d6
Refo Ilmiya <refo.ilmiya@gmail.com>
2023-08-01 14:52:57 +0300

    Reworked backend responses

--- backend/src/index.js
+++ backend/src/index.js
@@ -49,17 +49,19 @@ router.get('/api/weather/forecast', async ctx => {
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.list
     ? weatherData.list.map(x => ({
+      location: weatherData.city.name,
+      dt: x.dt,
       date_time_text: x.dt_txt,
       weather_id: x.weather[0].id,
       weather_main: x.weather[0].main,
       weather_icon: x.weather[0].icon,
-      temperature: x.main.temp,
-      feels_like: x.main.feels_like,
-      wind_speed: x.wind.speed,
+      temperature: (x.main.temp - 273.15).toFixed(0) + '°C',
+      feels_like: (x.main.feels_like - 273.15).toFixed(0) + '°C',
       wind_deg: x.wind.deg,
-      wind_gust: x.wind.gust,
-      rain_probability: x.pop,
-      rain: x.rain,
+      wind_speed: x.wind.speed + ' m/s',
+      wind_gust: x.wind.gust + ' m/s',
+      rain_probability: (x.pop * 100).toFixed(0) + '%',
+      rain_3h: (x.rain ? x.rain['3h'] : 0) + ' mm',
     }))
     : [];
 });
